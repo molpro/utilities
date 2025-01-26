5847c22663a74bb82ca4d0166672f34c9808328b
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-01-26 14:56:28 +0000

    Use new Lapack interface on MacOS

--- src/molpro/cblas.h
+++ src/molpro/cblas.h
@@ -9,8 +9,9 @@
 #include <mkl_cblas.h>
 #define HAVE_CBLAS
 #define HAVE_MKL
-#elif __has_include(<Accelerate/../Frameworks/vecLib.framework/Headers/cblas.h>)
-#include <Accelerate/../Frameworks/vecLib.framework/Headers/cblas.h>
+#elif __has_include(<Accelerate/Accelerate.h>)
+#define ACCELERATE_NEW_LAPACK
+#include <Accelerate/Accelerate.h>
 #define HAVE_CBLAS
 #define HAVE_ACCELERATE
 #elif __has_include(<cblas.h>)
