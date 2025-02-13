0dcdbd25990e6dc77b967491e518b9e802eec45e
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-02-13 06:59:42 +0000

    clean up MPI build and test

--- test/test-mpi.cpp
+++ test/test-mpi.cpp
@@ -2,5 +2,6 @@
 #include <molpro/mpi.h>
 
 TEST(mpi, construct) {
-  EXPECT_EQ(molpro::mpi::comm_global(),molpro::mpi::comm_self());
+  EXPECT_EQ(molpro::mpi::comm_global(),MPI_COMM_WORLD);
+  EXPECT_EQ(molpro::mpi::comm_self(),MPI_COMM_SELF);
 }
